6e07daa99c5caa46be4388feffebd4738153f9ff
Ian Schoenrock <Ian.Schoenrock@Transcendent.ai>
2019-01-09 15:40:46 -0500

    webview

--- app/src/main/java/com/example/ianschoenrock/layoutdesigner/MainActivity.kt
+++ app/src/main/java/com/example/ianschoenrock/layoutdesigner/MainActivity.kt
@@ -1,130 +1,45 @@
 package com.example.ianschoenrock.layoutdesigner
 
-import android.Manifest
-import android.content.Context
-import android.hardware.Sensor
+
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.hardware.SensorManager
-import android.os.Environment
-import android.widget.Toast
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
 import kotlinx.android.synthetic.main.activity_main.*
-import java.nio.file.Files.exists
-import android.os.Environment.getExternalStorageDirectory
-import java.io.File
-import java.io.FileNotFoundException
-import java.io.FileOutputStream
-import java.io.IOException
-import java.nio.file.Files.isDirectory
-import java.nio.file.Files.exists
-import android.Manifest.permission
-import android.Manifest.permission.SEND_SMS
-import android.content.pm.PackageManager
-import android.support.v4.app.ActivityCompat
-import android.util.Log
+import android.webkit.WebViewClient
 
 
-private val filename = "DangerousVersion3.txt"
-var stringBuffer: StringBuffer? = null
-var fileToReadAndWrite: File? = null
+
 
 class MainActivity : AppCompatActivity() {
-
-
-    private var sensorMan: SensorManager? = null
-    private var accelerometer: Sensor? = null
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val myWebView: WebView = findViewById(R.id.web_view)
+        myWebView.webViewClient = Callback()
 
-        val PERMISSION_REQUEST_CODE = 1
+        search_button.setOnClickListener {
 
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            var url = url_edit_text.text.toString()
 
-            if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+            if(!url.contains("https://www."))
+                url = "https://www.$url"
 
-                Log.d("permission", "permission denied to SEND_SMS - requesting it")
-                val permissions = arrayOf<String>(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-
-                requestPermissions(permissions, PERMISSION_REQUEST_CODE)
-
-            }
+            myWebView.loadUrl(url)
         }
 
+    }
 
+    private inner class Callback : WebViewClient() {  //HERE IS THE MAIN CHANGE.
 
-        button.setOnClickListener {
-            if (isExternalStorageWritable()) {
-                if (filesDir.exists()) {
-                    if (isFileWritable(filesDir)) {
-                        writeToFile(getFilename())
-                    } else {
-                        Toast.makeText(this@MainActivity, "Not Writable dir", Toast.LENGTH_SHORT).show()
-                    }
-                } else {
-                    Toast.makeText(this@MainActivity, "No directory", Toast.LENGTH_LONG).show()
-                }
-            } else {
-                Toast.makeText(this@MainActivity, "Storage not writable.", Toast.LENGTH_LONG).show()
-
-
-            }
+        override fun shouldOverrideUrlLoading(view: WebView, url: WebResourceRequest): Boolean {
+            return false
         }
 
-
-
-        ///storage/emulated/0/myfile
-
     }
 
-
-    private fun isExternalStorageWritable(): Boolean {
-        val state = Environment.getExternalStorageState()
-        return Environment.MEDIA_MOUNTED == state
-    }
-
-
-    private fun getDocumentStoragePath(): File {
-        // Get the directory for the user's public documents directory.
-        val path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS)
-        if (!path.mkdirs()) {
-            path.mkdirs()
-        }
-        return path
-    }
-
-
-    private fun getFilename(): File {
-        fileToReadAndWrite = File(getDocumentStoragePath(), filename)
-        return fileToReadAndWrite as File
-    }
-
-    private fun isFileWritable(file: File): Boolean {
-        return if (!file.canWrite()) {
-            file.setWritable(true)
-        } else file.canWrite()
-    }
-
-
-    private fun writeToFile(textFile: File) {
-        try {
-
-            val fos = FileOutputStream(textFile)
-            fos.write("Hello World I'm writing to a file".toByteArray())
-            fos.close()
-            Toast.makeText(this@MainActivity, "Finish writing...", Toast.LENGTH_SHORT).show()
-        } catch (e: FileNotFoundException) {
-            Toast.makeText(this@MainActivity, e.toString(), Toast.LENGTH_LONG).show()
-        } catch (e: IOException) {
-            Toast.makeText(this@MainActivity, e.toString(), Toast.LENGTH_LONG).show()
-        } finally {
-
-        }
-    }
 
 
 }
